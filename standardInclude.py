6ef093f48250dad21c3c366684511abf505049eb
joshua.labounty@gmail.com <joshua.labounty@gmail.com>
2019-12-05 16:04:27 -0800

    Adds function to read/write fcl files into python dictionaries

--- standardInclude.py
+++ standardInclude.py
@@ -29,6 +29,8 @@ import scipy
 import ROOT as r
 r.gStyle.SetOptStat(0)
 r.gStyle.SetOptFit(1111)
+r.gStyle.SetPalette(112)
+r.gStyle.SetNumberContours(80)
 
 #g-2 Blinding Software
 from BlindersPy3 import Blinders
@@ -1015,3 +1017,79 @@ def createSamDataset(name, runs, extraConditions = "", dataType = 'raw'):
     dataset += ') and data_tier '+str(dataType)+' " '
     
     return dataset
+
+#reads fcl file into a dictionary
+def fclReader(fileName):
+    fclDict = {}
+    tempDicts = []
+    tempKeys = []
+    dictCounter = 0
+    with open(str(fileName)) as file:
+        for line in file:
+            #print(line)
+            #if((":" in line or "}" in line or "{" in line) and ("//" not in line)):
+            if(("//" not in line) and ("PROLOG" not in line)):
+                #print(line)
+                if("{" in line):
+                    #print("start of new nested dict")
+                    newDict = {}
+                    tempDicts.append(newDict)
+                    key = line.split(":")[0].rstrip().lstrip()
+                    #print(key)
+                    tempKeys.append(key)
+                    dictCounter += 1
+                elif("}" in line):
+                    #print("end of nested dict")
+                    dictCounter -= 1
+                    if(len(tempDicts) > 1):
+                        thisKey = tempKeys[dictCounter]
+                        previousKey = tempKeys[dictCounter - 1]
+                        #print(thisKey, previousKey)
+                        tempDicts[dictCounter - 1][thisKey] = tempDicts[dictCounter]
+                        tempDicts = tempDicts[:-1]
+                        tempKeys = tempKeys[:-1]
+                    else:
+                        thisKey = tempKeys[dictCounter]
+                        fclDict[thisKey] = tempDicts[dictCounter]
+                        tempDicts = tempDicts[:-1]
+                        tempKeys = tempKeys[:-1]
+                        
+                elif(":" in line):
+                    #print(line.split(":"))
+                    key = line.split(":")[0].rstrip().lstrip()
+                    value = line.split(":")[1].rstrip().lstrip()
+                    try:
+                        vi = float(value)
+                        value = vi
+                    except:
+                        value = value
+                    #print(key, value)
+                    if(dictCounter > 0):
+                        tempDicts[dictCounter-1][key] = value
+                    else:
+                        fclDict[key] = value
+    return fclDict
+
+def printDictInFcl(d, file, tabCounter = 0):
+    tabString = ""
+    for i in range(tabCounter):
+        tabString+="\t"
+    for di in d:
+        di_val = d[di]
+        if(isinstance(di_val,dict)):
+            #print("nested")
+            file.writelines([tabString,di," : {","\n"])
+            printDictInFcl(di_val, file, tabCounter+1)
+            file.writelines([tabString,"}","\n"])
+        else:
+            file.writelines([tabString,di," : ", str(di_val),"\n"])
+
+#writes fcl to specified file
+def fclWriter(fclDict, fileName, comment = ""):
+    with open(fileName,"w") as file:
+        file.writelines(["// FCL Generated from python dictionary","\n"])
+        file.writelines(["// Comment: "+str(comment),"\n"])
+        file.writelines(["BEGIN_PROLOG","\n"])
+        printDictInFcl(fclDict, file)   
+        file.writelines(["END_PROLOG","\n"])
+    file.close()
