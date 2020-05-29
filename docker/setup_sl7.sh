ac5aa34cb78bf91c64bd91b1cb0bef1ab0d2279f
Josh LaBounty <jjlab@uw.edu>
2020-05-29 15:14:10 -0700

    Adds setup for SLF docker container

---
+++ docker/setup_sl7.sh
@@ -0,0 +1,7 @@
+source /cvmfs/gm2.opensciencegrid.org/prod/g-2/setup
+export PRODUCTS=/cvmfs/gm2.opensciencegrid.org/specials/sl7/prod/g-2:/cvmfs/gm2.opensciencegrid.org/specials/sl7/prod/external:$PRODUCTS
+setup gm2 v9_44_00 -q prof
+
+cd ./nearline_sl7/
+source localProducts_gm2_v9_44_00_prof/setup 
+. mrb s 
